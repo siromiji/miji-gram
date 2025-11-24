ca6d9c0df33be52e0491b75d3531504352c27244
siromiji <mnmn82018282@gmail.com>
2025-11-24 09:42:22 +0900

    init

---
+++ server/app.js
@@ -0,0 +1,22 @@
+/**
+ * @file app.js
+ * @description Entry Point
+ * 251117 v1.0.0 park
+ */
+
+import express from 'express';
+import './configs/env.config.js';
+import authRouter from './routes/auth.router.js';
+
+const app = express();
+app.use(express.json()); // JSON 요청 파싱 처리
+
+// --------------------
+// 라우터 정의
+// --------------------
+app.use('/api/auth', authRouter);
+
+// --------------------
+// 해당 Port로 express 실행
+// --------------------
+app.listen(parseInt(process.env.APP_PORT));
